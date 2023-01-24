57b42c6f3fb5c3f08288384421c0b8d3d351a27c
LoneStrategos <fa-bohr@t-online.de>
2023-01-24 19:27:04 +0100

    Version 24.01.2023b
    -Working Title Screen
    -Explosions

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    Text startGameText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startGameText = GetComponentInChildren<Text>();
+        StartFlashing();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            StartGame();
+        }
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("InGame");
+    }
+
+    IEnumerator StartGameFlickerRoutine()
+    {
+        while (true)
+        {
+            startGameText.text = "Start Game!";
+            yield return new WaitForSeconds(0.35f);
+            startGameText.text = "";
+            yield return new WaitForSeconds(0.35f);
+        }
+    }
+
+    void StartFlashing()
+    {
+        StopCoroutine ("StartGameFlickerRoutine");
+        StartCoroutine("StartGameFlickerRoutine");
+    }
+
+    void StopFlashing()
+    {
+        StopCoroutine("StartGameFlickerRoutine");
+    }
+}
